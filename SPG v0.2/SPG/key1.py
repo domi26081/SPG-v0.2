7dd7f1449d996ed4f66942470efa4fec08a36841
domi26081 <94846683+domi26081@users.noreply.github.com>
2022-01-27 08:28:56 +0100

    Add files via upload

---
+++ SPG v0.2/SPG/key1.py
@@ -0,0 +1,9 @@
+-########################\MENU/###########################-
+(1) Random Passwords Generator [NEW]
+(2) Custome Spg
+(3) Help & Info
+(4) Quit Program
+(5) Restar
+(6) Test
+(7) Axh [Alghoritm]
+-#########################################################-
